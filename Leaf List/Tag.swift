9788cadaa026cd6ca0d0a30216f4204f5eac0426
gregpaton08 <gregpaton08@gmail.com>
2017-07-14 11:49:23 -0400

    First commit

---
+++ Leaf List/Tag.swift
@@ -0,0 +1,14 @@
+//
+//  Tag.swift
+//  Leaf List
+//
+//  Created by Greg Paton on 7/13/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Tag: NSManagedObject {
+
+}
