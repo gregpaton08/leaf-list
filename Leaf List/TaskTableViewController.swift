d1a27768b4b4c80e75684e79d8e43a824b094e61
gregpaton08 <gregpaton08@gmail.com>
2017-07-16 16:33:17 -0400

    Add segue to another task view when a task is selected

--- Leaf List/TaskTableViewController.swift
+++ Leaf List/TaskTableViewController.swift
@@ -115,7 +115,6 @@ class TaskTableViewController: UITableViewController, UITextFieldDelegate {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -123,6 +122,5 @@ class TaskTableViewController: UITableViewController, UITextFieldDelegate {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
