edcb567efd97ea6c464d3f1deb66afc14eebef32
gregpaton08 <gregpaton08@gmail.com>
2017-07-31 21:44:27 -0400

    Fix crash case

---
+++ Leaf List/TaskTableViewFooter.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewFooter.swift
+//  Leaf List
+//
+//  Created by Greg Paton on 7/31/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewFooter: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
