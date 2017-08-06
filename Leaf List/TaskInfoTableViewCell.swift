a082b0f78c043e8622bb54cf3799724bc081922c
gregpaton08 <gregpaton08@gmail.com>
2017-08-06 14:53:34 -0400

    Add sublcass for task info cell

---
+++ Leaf List/TaskInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskInfoTableViewCell.swift
+//  Leaf List
+//
+//  Created by Greg Paton on 8/6/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class TaskInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
