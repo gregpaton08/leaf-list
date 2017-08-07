f22b7697c3fda215d213093fda8a1dc9931d9974
gregpaton08 <gregpaton08@gmail.com>
2017-08-07 10:15:38 -0400

    Update details view controller API

--- Leaf List/DetailsTableViewController.swift
+++ Leaf List/DetailsTableViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsTableViewController: UITableViewController {
 
+    // MARK: - API
+    
+    var task: Task?
+    
+    // MARK: - UI
+    
     @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var dateCreatedCell: UITableViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +29,8 @@ class DetailsTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        taskNameTextField.text = "TESTING"
+        taskNameTextField.text = task?.name ?? "ERROR!"
+        dateCreatedCell.textLabel?.text = formatDate(task?.dateCreated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,71 +38,28 @@ class DetailsTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: {
-//            switch indexPath.row
-//            {
-//            case 0:
-//                return "taskNameCell"
-//            case 1:
-//                return "taskNotesCell"
-//            case 2:
-//                return "taskDateCreatedCell"
-//            default:
-//                return ""
-//            }
-//        }(), for: indexPath)
-//
-//        // Configure the cell...
-//
-//        return cell
-//    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let notesView = segue.destination as? NotesViewController {
+            notesView.task = task
+        }
     }
-    */
+    
+    // MARK: - Supporting functions
+    
+    private func formatDate(_ date: NSDate?) -> String {
+        if date == nil {
+            return "ERROR!"
+        }
+        // TODO: compare to current date:
+        //     * If year is the same, do not display the year
+        //     * If day is the same then display 'today' instead of the date (or '10 minutes ago'?)
+        let formatter = DateFormatter()
+        formatter.locale = Locale.init(identifier: "en_US")
+        formatter.setLocalizedDateFormatFromTemplate("MMMMdyyyyHH:mma")
+        return "Created on " + formatter.string(from: (date as Date?)!)
+    }
+
 
 }
