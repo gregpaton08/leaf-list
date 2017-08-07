1f38648bfdf8e79b4d9816adafe56daf7f6cd079
gregpaton08 <gregpaton08@gmail.com>
2017-08-07 10:16:14 -0400

    Remove unused code

--- Leaf List/DetailsTableViewController.swift
+++ Leaf List/DetailsTableViewController.swift
@@ -22,20 +22,9 @@ class DetailsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
         taskNameTextField.text = task?.name ?? "ERROR!"
         dateCreatedCell.textLabel?.text = formatDate(task?.dateCreated)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Navigation
