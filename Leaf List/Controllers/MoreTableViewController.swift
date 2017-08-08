a559a279dc897b1976339fcb7d85e9e23d436314
gregpaton08 <gregpaton08@gmail.com>
2017-08-07 21:21:33 -0400

    Remove unused code

--- Leaf List/Controllers/MoreTableViewController.swift
+++ Leaf List/Controllers/MoreTableViewController.swift
@@ -9,28 +9,8 @@
 import UIKit
 
 class MoreTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
         if let taskVC = segue.destination as? TaskTableViewController {
             taskVC.displayStyle = .trash
             taskVC.showCompleted = true
