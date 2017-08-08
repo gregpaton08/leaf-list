aa1d69b0e270ce2883529e7c2ef260f66208b60a
gregpaton08 <gregpaton08@gmail.com>
2017-08-08 12:56:59 -0400

    Update check box class

--- Leaf List/Views/CheckBox.swift
+++ Leaf List/Views/CheckBox.swift
@@ -11,40 +11,34 @@ import UIKit
 @IBDesignable
 class CheckBox: UIButton {
 
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
-        
+        // Draw the checkbox border.
         let path = UIBezierPath()
         path.addArc(withCenter: self.center, radius: 11.0, startAngle: 0, endAngle: 2.0 * CGFloat.pi, clockwise: false)
-        UIColor.defaultButtonBlue.setStroke()
+        buttonColor.setStroke()
         path.lineWidth = 1.0
         path.stroke()
         
+        // Draw the center of the checkbox if checked.
         if isChecked {
             let path = UIBezierPath()
             path.addArc(withCenter: self.center, radius: 9.0, startAngle: 0, endAngle: 2.0 * CGFloat.pi, clockwise: false)
-            UIColor.defaultButtonBlue.setFill()
-//            path.lineWidth = 1.0
+            buttonColor.setFill()
             path.fill()
         }
     }
  
     @IBInspectable var isChecked : Bool = false {
         didSet {
-//            self.layer.backgroundColor = isChecked ? checkedBoxColor.cgColor : UIColor.clear.cgColor
             setNeedsDisplay()
         }
     }
     
-    @IBInspectable var borderColor: UIColor? {
+    @IBInspectable var buttonColor: UIColor = UIColor.defaultButtonBlue {
         didSet {
-//            layer.borderColor = borderColor?.cgColor
+            setNeedsDisplay()
         }
     }
-    
-    @IBInspectable var checkedBoxColor: UIColor = UIColor.defaultButtonBlue
     
     func toggleState() {
         isChecked = !isChecked
@@ -52,17 +46,6 @@ class CheckBox: UIButton {
     
     override var frame: CGRect {
         didSet {
-            // TODO: hard code radius to 11 (width = height = 22).
-//            self.layer.frame.size.width = 22.0
-//            self.layer.frame.size.height = 22.0
-            
-//            self.layer.frame.origin.x = (self.bounds.height - self.layer.bounds.height) / 2
-//            self.layer.frame.origin.y = (self.bounds.width - self.layer.bounds.width) / 2
-            
-//            self.layer.cornerRadius = 11.0
-//            self.layer.borderWidth = 1.0
-//            self.layer.borderColor = UIColor.defaultButtonBlue.cgColor
-//            self.layer.backgroundColor = isChecked ? checkedBoxColor.cgColor : UIColor.clear.cgColor
             titleLabel?.text = nil
             titleLabel?.isHidden = true
         }
