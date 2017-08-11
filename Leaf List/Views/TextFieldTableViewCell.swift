8640762670e2e13eb5ea7a24a76d9a388efc003b
gregpaton08 <gregpaton08@gmail.com>
2017-08-11 08:24:35 -0400

    Add code for a table view cell with a text field

---
+++ Leaf List/Views/TextFieldTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextFieldTableViewCell.swift
+//  Leaf List
+//
+//  Created by Greg Paton on 8/11/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
