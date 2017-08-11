a06ef9e28487d54f9e3435c055bd0c4b73629c4d
gregpaton08 <gregpaton08@gmail.com>
2017-08-11 09:01:22 -0400

    Add alert when trash is about to be emptied

--- Leaf List/Controllers/MoreTableViewController.swift
+++ Leaf List/Controllers/MoreTableViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class MoreTableViewController: UITableViewController {
     
+    @IBAction func emptyTrashButtonPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Empty Trash?", message: "Are you sure you want to empty the trash? This action cannot be undone.", preferredStyle: .alert)
+        let noAction = UIAlertAction(title: "No", style: .cancel) { (action) in
+            
+        }
+        let yesAction = UIAlertAction(title: "Yes", style: .destructive) { (action) in
+            // Delete tasks in trash
+        }
+        alertController.addAction(noAction)
+        alertController.addAction(yesAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let taskVC = segue.destination as? TaskTableViewController {
             taskVC.displayStyle = .trash
