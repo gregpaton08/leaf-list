1293e45c514bc59a49b198eb455b5221e73985fd
gregpaton08 <gregpaton08@gmail.com>
2017-12-08 18:29:22 -0500

    Update data model

---
+++ Leaf List/Models/Group.swift
@@ -0,0 +1,14 @@
+//
+//  Group.swift
+//  Leaf List
+//
+//  Created by Greg Paton on 12/8/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Group: NSManagedObject {
+
+}
